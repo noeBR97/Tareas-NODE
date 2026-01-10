e88c5db70465020f71621ae7d27b38ca2f57c530
Noelia Barrionuevo <noe.br97@gmail.com>
2026-01-10 20:07:42 +0100

    fix. añado campos a usuario

--- backend/models/usuario.js
+++ backend/models/usuario.js
@@ -4,6 +4,8 @@ import { v4 as uuidv4 } from 'uuid';
 const usuarioSchema = new mongoose.Schema({
     id: uuidv4(),
     nombre: {type: String, required: true},
+    apellido1: {type: String, required: true},
+    apellido2: {type: String},
     email: {type: String, unique: true, required: true},
     password: {type: String, required: true},
     rol: {type: String, enum: ['admin', 'usuario'], default: 'usuario'}
