f5e6de97927d279045d7eab19f8a1ef49bf413cf
Noelia Barrionuevo <noe.br97@gmail.com>
2026-01-10 19:51:45 +0100

    fix: campo rol a tabla usuarios

--- backend/models/usuario.js
+++ backend/models/usuario.js
@@ -6,6 +6,7 @@ const usuarioSchema = new mongoose.Schema({
     nombre: {type: String, required: true},
     email: {type: String, unique: true, required: true},
     password: {type: String, required: true},
+    rol: {type: String, enum: ['admin', 'usuario'], default: 'usuario'}
 }, { collection: 'usuarios', versionKey: false, strict: false });
 
 const Usuario = mongoose.model('Usuario', usuarioSchema);
