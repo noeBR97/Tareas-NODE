923fa24f5435cc54ed3b9e981bba7bbf4b2e12d2
Noelia Barrionuevo <noe.br97@gmail.com>
2026-01-12 12:31:20 +0100

    fix. cambio de nombre de archivo y refactorizacion

---
+++ backend/controllers/authController.js
@@ -0,0 +1,65 @@
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
+import Usuario from '../models/usuario.js';
+
+export const registro = async (req, res) => {
+    const {nombre, email, password} = req.body
+
+    try {
+        //Verificar si el usuario ya existe
+        const usuarioExistente = await Usuario.findOne({email});
+        if (usuarioExistente) {
+            return res.status(400).json({mensaje: 'El usuario ya existe'});
+        }
+
+        //Verificar si faltan campos obligatorios
+        if (!nombre || !email || !password) {
+            return res.status(400).json({mensaje: 'Faltan campos obligatorios'});
+        }
+
+        const hashPassword = await bcrypt.hash(password, 10);
+
+        const nuevoUsuario = new Usuario({
+            nombre,
+            email,
+            password: hashPassword
+        });
+        await nuevoUsuario.save();
+
+        res.status(201).json({mensaje: 'Usuario registrado correctamente', nuevoUsuario});
+    } catch (error) {
+        res.status(500).json({mensaje: 'Error al registrar el usuario', error});
+    }
+}
+
+export const login = async (req, res) => {
+    try {
+        const {email, password} = req.body;
+
+        //comprobar si faltan campos
+        if (!email || !password) {
+            return res.status(400).json({mensaje: 'Faltan campos obligatorios'});
+        }
+
+        const usuario = await Usuario.findOne({email});
+        //comprobar si existe el usuario
+        if (!usuario) {
+            return res.status(400).json({mensaje: 'Credenciales inválidas'});
+        }
+
+        const esPasswordValido = await bcrypt.compare(password, usuario.password);
+        //comprobar si la contraseña es correcta
+        if (!esPasswordValido) {
+            return res.status(400).json({mensaje: 'Credenciales inválidas'});
+        }
+
+        const token = jwt.sign(
+            {id: usuario._id, email: usuario.email, rol: usuario.rol},
+            process.env.JWT_SECRET,
+            {expiresIn: '2h'}
+        )
+        res.status(200).json({mensaje: 'Login exitoso', token})
+    } catch (error) {
+        res.status(500).json({mensaje: 'Error al iniciar sesión', error});
+    }
+}
