a19b93858b478602252d5decf15f16c78b7d3c0f
Noelia Barrionuevo <noe.br97@gmail.com>
2026-01-26 23:17:40 +0100

    rutas

--- backend/routes/tareaRoutes.js
+++ backend/routes/tareaRoutes.js
@@ -7,7 +7,18 @@ export const router = Router();
 //middleware que se aplica a todas las rutas
 router.use(validarJWT)
 
-//admin
+//rutas de usuario
+router.get('/por-hacer', esUsuario, controlador.getTareasPorHacer) 
+router.get('/asignadas', esUsuario, controlador.getTareasAsignadas)
+router.put('/asignar-propia/:idTarea', esUsuario, controlador.asignarTareaPropia)
+router.put('/desasignar-propia/:idTarea', esUsuario, controlador.desasignarTareaPropia)
+router.put('/cambiar-estado/:idTarea', esUsuario, controlador.cambiarEstado)
+router.get('/:dificultad', esUsuario, controlador.getTareaByDificultad)
+router.get('/:min/:max', esUsuario, controlador.getTareasPorRango)
+router.get('/dificultad-xl', esUsuario, controlador.getTareasXL)
+router.get('/:idUsuario', esUsuario, controlador.getTareasByUsuario)
+
+//rutas de admin
 router.get('/', esAdmin, controlador.getAllTareas)
 router.get('/:idTarea', esAdmin, controlador.getTareaById)
 router.post('/', esAdmin, controlador.addTarea)
@@ -15,17 +26,3 @@ router.put('/:id', esAdmin, controlador.updateTarea)
 router.delete('/:id', esAdmin, controlador.deleteTarea)
 router.put('/asignar/:idTarea/:idUsuario', esAdmin, controlador.asignarTarea)
 router.post('/llenar/:numUsarios', esAdmin, controlador.llenarSistema)
-// //tareas por realizar
-// router.get('/por-hacer', obtenerTareasPorHacer) 
-
-// //tareas asignadas
-// router.get('/asignadas', obtenerTareasAsignadas)
-
-// //asignar tarea
-// router.post('/asignar', asignarTarea)
-
-// //desasignar tarea
-// router.post('/desasignar', desasignarTarea)
-
-// //cambiar estado de tarea
-// router.post('/cambiar-estado', cambiarEstadoTarea)
